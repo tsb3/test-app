3114a6ef196a8228b352fa27f9edec64de555b61
tsubasa <tsubasa.ito111@gmail.com>
2019-09-16 12:43:52 +0000

    appplication.js updated

--- Gemfile
+++ Gemfile
@@ -51,5 +51,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 
 group :production do
-gem 'pg', '~> 0.11'
+  gem 'pg', '~> 0.11'
 end
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,8 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs
+//= require jquery-ui
+//= require rails-ujs
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
